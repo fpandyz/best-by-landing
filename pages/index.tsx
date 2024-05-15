0fc8e3263320db585fd8f8b132d94c734d9e1128
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-15 11:30:45 +0500

    style: removing unnecessary fonts and styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import Head from 'next/head';
-import { Inter, Montserrat, Manrope } from 'next/font/google';
+import { Inter, Manrope } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 
 const inter = Inter({
   subsets: ['latin', 'cyrillic'],
-  weight: ['400', '500', '600'],
-});
-
-const montserrat = Montserrat({
-  subsets: ['latin', 'cyrillic'],
-  weight: ['500', '700'],
+  weight: ['500', '600'],
 });
 
 const manrope = Manrope({
@@ -28,20 +23,11 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div>
-          <p className={`${inter.className}`} style={{ fontWeight: 400 }}>Inter 400</p>
           <p className={`${inter.className}`} style={{ fontWeight: 500 }}>Inter 500</p>
           <p className={`${inter.className}`} style={{ fontWeight: 600 }}>Inter 600</p>
 
-          <p className={`${montserrat.className}`} style={{ fontWeight: 500 }}>Montserrat 500</p>
-          <p className={`${montserrat.className}`} style={{ fontWeight: 700 }}>Montserrat 700</p>
-
           <p className={`${manrope.className}`} style={{ fontWeight: 400 }}>Manrope 400</p>
           <p className={`${manrope.className}`} style={{ fontWeight: 500 }}>Manrope 500</p>
-
-          <p className="arialRegular">Arial 400</p>
-
-          <p className="helveticaLight">Helvetica 300</p>
-          <p className="helveticaRegular">Helvetica 400</p>
         </div>
       </main>
     </>
