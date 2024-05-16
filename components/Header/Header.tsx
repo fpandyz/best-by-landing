691e3be80c132be8ec264bb7c588ee0d2c0dcac1
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-16 21:22:33 +0500

    feat: creating a Header Component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__logo" />
+        <a href="mailto:contact@tourmalinecore.com" className="header__btnPost">Оставить заявку</a>
+      </div>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
+import { Header } from '../components/Header/Header';
 
 export default function Home() {
   return (
-    <Head>
-      <title>Best By</title>
-      <meta name="description" content="Service for tracking product expiration dates" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>Best By</title>
+        <meta name="description" content="Service for tracking product expiration dates" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+    </>
   );
 }
