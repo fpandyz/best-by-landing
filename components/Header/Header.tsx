74833c01f29bfd6aa0a838345b8edd11843e1816
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-17 08:05:25 +0500

    style: styling the header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
+import LogoImageMobileSrc from '../../public/images/MINI_LOGO.svg';
+import LogoImageDesktopSrc from '../../public/images/LOGO.svg';
+
 export function Header() {
   return (
     <header className="header">
-      <div className="container">
-        <div className="header__logo" />
+      <div className="container header__wrapper">
+        <Image
+          src={LogoImageMobileSrc}
+          className="header__logo header__logo--small"
+          alt="headerLogo"
+        />
+        <Image
+          src={LogoImageDesktopSrc}
+          className="header__logo header__logo--desktop"
+          alt="headerLogo"
+        />
         <a href="mailto:contact@tourmalinecore.com" className="header__btnPost">Оставить заявку</a>
       </div>
     </header>
