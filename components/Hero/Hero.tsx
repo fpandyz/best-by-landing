79b2a548654d6a0910cac3795993a4bfa10da5c1
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-18 16:51:18 +0500

    style: styling the hero component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,8 +1,36 @@
+import Image from 'next/image';
+import SeafoodImageSrc from '../../public/images/seafood.jpg';
+import PreviewInterfaceSrc from '../../public/images/previewInterface1.jpg';
+import CakeSrc from '../../public/images/cake.jpg';
+
 export function Hero() {
   return (
     <div className="hero">
-      <div className="container">
-        Hero
+      <div className="container hero__wrapper">
+        <p className="font-family-2 text-type-1 hero__title">
+          Сервис для отслеживания срока годности продукции
+        </p>
+        <p className="font-family-1 text-type-5 hero__description">
+          Best By помогает продуктовым сетям и точкам общепита сохранять
+          на прилавках только свежие и безопасные продукты
+        </p>
+        <div className="hero__cards">
+          <Image
+            src={SeafoodImageSrc}
+            className="hero__cardImage"
+            alt="Seafood Image"
+          />
+          <Image
+            src={PreviewInterfaceSrc}
+            className="hero__cardImage hero__cardImage_large"
+            alt="Preview Interaface Image"
+          />
+          <Image
+            src={CakeSrc}
+            className="hero__cardImage"
+            alt="Cake Image"
+          />
+        </div>
       </div>
     </div>
   );
