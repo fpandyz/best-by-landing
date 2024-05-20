12e1f2ad5676c6aaaf53e4e0c63f348f7ef2bbfb
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-20 11:23:09 +0500

    feat: creating a Benefit Component

---
+++ components/Benefit/Benefit.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import DiagramSrc from '../../public/images/diagram.svg';
+import CustomersSrc from '../../public/images/customers.png';
+
+export function Benefit() {
+  return (
+    <section className="benefit">
+      <div className="font-family-1 container benefit__wrapper">
+        <div className="benefit__card benefit__card--first">
+          <h2 className="font-family-2 text-type-2 benefit__block-title">Чем полезен</h2>
+        </div>
+        <div className="benefit__card benefit__card--last">
+          <p className="text-type-6 benefit__description">
+            Вам и вашим сотрудникам больше не нужно проверять свежесть продукции вручную.
+            Best By поможет сосредоточиться только на продукции требующей вашего вниманияl
+          </p>
+        </div>
+        <div className="benefit__card">
+          <p className="text-type-4 benefit__title benefit__title--short">Уменьшает убытки</p>
+          <p className="text-type-6 benefit__description">Сокращает списания товаров с истекшим сроком годности</p>
+          <Image
+            src={DiagramSrc}
+            className="benefit__diagram"
+            alt="Diagram Image"
+          />
+        </div>
+        <div className="benefit__card benefit__card--image-bg-1">
+          <p className="text-type-4 benefit__title">Помогает контролировать работу сотрудников</p>
+        </div>
+        <div className="benefit__card">
+          <p className="text-type-4 benefit__title">Повышает доверие покупателей</p>
+          <p className="text-type-6 benefit__description">Отслеживает продукцию, попадающую на прилавок</p>
+          <Image
+            src={CustomersSrc}
+            className="benefit__customers"
+            alt="Customer Trust Image"
+          />
+        </div>
+        <div className="benefit__card benefit__card--image-bg-2">
+          <p className="text-type-4 benefit__title">Снижает риск продажи просроченной продукции</p>
+        </div>
+      </div>
+    </section>
+  );
+}
