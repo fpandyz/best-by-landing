244e408229315b8bb1392a73aec07029572b2688
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-20 09:54:44 +0500

    style: adjusting font connections

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/index.scss';
 import type { AppProps } from 'next/app';
+import { Inter, Manrope } from 'next/font/google';
+
+const inter = Inter({
+  weight: ['500', '600'],
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-inter',
+});
+
+const manrope = Manrope({
+  weight: ['400', '500'],
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-manrope',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${inter.className} ${manrope.className}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
