ed2875181cdb90a038e046f35d70be1222d10fd8
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-24 15:36:23 +0500

    refactor: changing class names

--- components/Possibilities/Possibilities.tsx
+++ components/Possibilities/Possibilities.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import Image from 'next/image';
 import BarcodeImageSrc from '../../public/images/possibilities-barcode.jpg';
 import ProductCardWhiteImageSrc from '../../public/images/possibilities-product-card-white.jpg';
-import ProductCardExpiresSrc from '../../public/images/possibilities-status-card-expires.jpg';
-import ProductCardExpiredSrc from '../../public/images/possibilities-status-card-expiredjpg.png';
+import ProductCardExpiresSrc from '../../public/images/possibilities-status-expires.jpg';
+import ProductCardExpiredSrc from '../../public/images/possibilities-status-expired.jpg';
 import TieIconSrc from '../../public/images/possibilities-tie.svg';
 import CapIconSrc from '../../public/images/possibilities-cap.svg';
 import InterfaceImageSrc from '../../public/images/possibilities-interface.jpg';
@@ -19,7 +20,7 @@ export function Possibilities() {
               Сканируйте штрихкод продукции и она сразу появится в системе.
               Вам останется только ввести срок годности и свойства продукта
             </p>
-            <div className="possibilities__product-images">
+            <div className="possibilities__barcode-images">
               <Image
                 src={BarcodeImageSrc}
                 className="possibilities__image"
@@ -38,10 +39,12 @@ export function Possibilities() {
               Если у вас большое производство, продукцию можно разделить по цехам
               и отслеживать свежесть продукции в каждом из них
             </p>
-            <span className="text-type-4 possibilities__category possibilities__category--red">Горячий</span>
-            <span className="text-type-4 possibilities__category">Холодный</span>
-            <span className="text-type-4 possibilities__category">Рыбный</span>
-            <span className="text-type-4 possibilities__category">Мясной</span>
+            <div className="possibilities__categories">
+              <span className="text-type-4 possibilities__category possibilities__category--red">Горячий</span>
+              <span className="text-type-4 possibilities__category">Холодный</span>
+              <span className="text-type-4 possibilities__category">Рыбный</span>
+              <span className="text-type-4 possibilities__category">Мясной</span>
+            </div>
           </div>
           <div className="possibilities__card">
             <span className="text-type-3 possibilities__label">
@@ -51,17 +54,17 @@ export function Possibilities() {
               У вас останется достаточное количество времени для снятия продукта с
               истекшим сроком годности с продажи, чтобы он не попал в руки покупателей
             </p>
-            <div className="possibilities__state-images">
-              <div className="possibilities__state-product">
-                <span className="font-family-3 possibilities__state-title">Истекает срок годности</span>
+            <div className="possibilities__product-images">
+              <div className="possibilities__state-card">
+                <span className="font-family-3 possibilities__status-label">Истекает срок годности</span>
                 <Image
                   src={ProductCardExpiresSrc}
                   className="possibilities__image"
                   alt="Product Status Image"
                 />
               </div>
-              <div className="possibilities__state-product">
-                <span className="font-family-3 possibilities__state-title">Просрочен</span>
+              <div className="possibilities__state-card">
+                <span className="font-family-3 possibilities__status-label">Просрочен</span>
                 <Image
                   src={ProductCardExpiredSrc}
                   className="possibilities__image"
@@ -70,7 +73,14 @@ export function Possibilities() {
               </div>
             </div>
           </div>
-          <div className="possibilities__card" />
+          <video
+            className="possibilities__card"
+            src="/video/possibilities-how-does-it-work.mp4"
+            playsInline
+            muted
+            autoPlay
+            loop
+          />
           <div className="possibilities__card">
             <span className="text-type-3 possibilities__label">Управление аккаунтами</span>
             <p className="text-type-6 possibilities__description">
