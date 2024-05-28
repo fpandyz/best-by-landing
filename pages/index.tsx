810190daa3be9b8f7a89d6f5551c7e2f3893b0b8
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-28 11:26:56 +0500

    chore: add favicons files

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <title>Best By</title>
         <meta name="description" content="Service for tracking product expiration dates" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <Header />
       <Layout>
