ba8a41fd6d06d9d161dd5d5cef15b4a7ed1b4407
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-28 08:24:01 +0500

    refactor: replacing div tags with heading and paragraph tags

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -4,11 +4,11 @@ export function Form() {
       <div className="container form__wrapper">
         <div className="form__image" />
         <div className="font-family-1 form__card">
-          <div className="font-family-2 text-type-2 form__title">Свяжитесь с нами</div>
-          <div className="text-type-5 form__description">
+          <h2 className="font-family-2 text-type-2 form__title">Свяжитесь с нами</h2>
+          <p className="text-type-5 form__description">
             Мы расскажем, как происходит внедрение системы
             в производственный процесс и ответим на все вопросы.
-          </div>
+          </p>
           <a href="mailto:contact@tourmalinecore.com" className="form__btn">Написать на почту</a>
         </div>
       </div>
