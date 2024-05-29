968350564c1b794b15d5d9eb357e68cb89ba63c9
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-05-29 13:18:22 +0500

    style: change connection font-family

--- components/Benefit/Benefit.tsx
+++ components/Benefit/Benefit.tsx
@@ -5,19 +5,19 @@ import CustomersSrc from '../../public/images/customers.png';
 export function Benefit() {
   return (
     <section className="benefit">
-      <div className="font-family-1 container benefit__wrapper">
+      <div className="container benefit__wrapper">
         <div className="benefit__card">
           <h2 className="font-family-2 text-type-2 benefit__block-title">Чем полезен</h2>
         </div>
         <div className="benefit__card">
-          <p className="benefit__description">
+          <p className="font-family-1 benefit__description">
             Вам и вашим сотрудникам больше не нужно проверять свежесть продукции вручную.
             Best By поможет сосредоточиться только на продукции требующей вашего внимания
           </p>
         </div>
         <div className="benefit__card">
-          <h3 className="text-type-4 benefit__title benefit__title--short">Уменьшает убытки</h3>
-          <p className="text-type-6 benefit__description">Сокращает списания товаров с истекшим сроком годности</p>
+          <h3 className="font-family-1 text-type-4 benefit__title benefit__title--short">Уменьшает убытки</h3>
+          <p className="font-family-1 text-type-6 benefit__description">Сокращает списания товаров с истекшим сроком годности</p>
           <Image
             src={DiagramSrc}
             className="benefit__diagram"
@@ -25,11 +25,11 @@ export function Benefit() {
           />
         </div>
         <div className="benefit__card">
-          <h3 className="text-type-4 benefit__title">Помогает контролировать работу сотрудников</h3>
+          <h3 className="font-family-1 text-type-4 benefit__title">Помогает контролировать работу сотрудников</h3>
         </div>
         <div className="benefit__card">
-          <h3 className="text-type-4 benefit__title">Повышает доверие покупателей</h3>
-          <p className="text-type-6 benefit__description">Отслеживает продукцию, попадающую на прилавок</p>
+          <h3 className="font-family-1 text-type-4 benefit__title">Повышает доверие покупателей</h3>
+          <p className="font-family-1 text-type-6 benefit__description">Отслеживает продукцию, попадающую на прилавок</p>
           <Image
             src={CustomersSrc}
             className="benefit__customers"
@@ -37,7 +37,7 @@ export function Benefit() {
           />
         </div>
         <div className="benefit__card">
-          <h3 className="text-type-4 benefit__title">Снижает риск продажи просроченной продукции</h3>
+          <h3 className="font-family-1 text-type-4 benefit__title">Снижает риск продажи просроченной продукции</h3>
         </div>
       </div>
     </section>
