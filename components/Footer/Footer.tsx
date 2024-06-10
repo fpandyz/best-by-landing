da28fa569c1b536a8089505e96ab1c91b93ff3d8
Artem Sheptunov <sheptunov-aa@mail.ru>
2024-06-10 13:30:36 +0500

    feat: add date update functionality

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react';
+
 export function Footer() {
+  const [date, setDate] = useState<string>();
+
+  useEffect(() => {
+    const year = String(new Date().getFullYear());
+
+    if (year === '2024') {
+      setDate(year);
+    } else {
+      setDate(`2024-${year}`);
+    }
+  }, []);
+
   return (
     <footer className="footer">
       <div className="font-weight-medium container footer__wrapper">
-        <span className="footer__text">© Best By 2024</span>
+        <span className="footer__text">
+          {`© Best By ${date}`}
+        </span>
         <a href="mailto:contact@tourmalinecore.com" className="footer__text">contact@tourmalinecore.com</a>
       </div>
     </footer>
